1fe8d5d8e142602c5c089ab759039d029ef2ff8f
Yonatan Amir <jonathanamir1@gmail.com>
2023-01-28 15:14:36 +0100

    movies created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'creating movies'
+
+response = HTTParty.get('http://tmdb.lewagon.com/movie/top_rated')
+response['results'].first(10).each do |result|
+  title = result['original_title']
+  overview = result['overview']
+  base_url = 'https://image.tmdb.org/t/p/w500'
+  poster_url = "#{base_url}#{result['poster_path']}"
+  rating = result['vote_average']
+  Movie.create!(title: title, overview: overview, poster_url: poster_url, rating: rating)
+end
+puts 'movies done successfully'
+
